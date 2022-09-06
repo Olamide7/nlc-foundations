62376fe40334dc3a9c5686925d9bb33e939a564c
olamide <uthmanola7@gmail.com>
2022-09-06 13:36:36 +0100

    Updating my Repository

---
+++ C_intro/README.md
@@ -0,0 +1,3 @@
+# Creating a C program and compiling it with gcc.
+# My first actually.
+# Thank you
---
+++ C_intro/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define AGE 50
+/**
+ * @brief program entry point
+ *
+ * @return int 0
+ */
+ int main (void)
+ {
+    printf("Hello World!\n");
+    printf("%i\n", AGE);
+    return (0);
+ }
